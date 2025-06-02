b73c186cb85b5e5e38edbf4b78cc38d750f4571c
Octopengi <octopengi@gmail.com>
2025-06-02 16:56:46 -0400

    Added Elevators

--- VIPER/Classes.py
+++ VIPER/Classes.py
@@ -3,4 +3,12 @@ class Elevator:
         self.num = num
         self.floor = 1
         self.direction = "None"
-        self.requests = {}
+        self.requests = {1: [], 2:[], 3:[], 4:[], 5:[], 6:[]}
+
+class ElevManag:
+    def __init__(self):
+        self.requests = {1: [], 2:[], 3:[], 4:[], 5:[], 6:[]}
+
+    def AddRequest(self, floor, direction):
+        if direction not in self.requests[floor]:
+            self.requests[floor].append(direction)
