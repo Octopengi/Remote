6bb4606ce2c076fc8492416b234db6d8d4a7c96d
Octopengi <octopengi@gmail.com>
2025-06-03 10:53:32 -0400

    asdf

--- Cards/classes.py
+++ Cards/classes.py
@@ -5,23 +5,54 @@ import functions as Func
 class GameMaster:
     def __init__(self):
         self.suites = ["Diamond", "Heart", "Spade", "Club"]
-        self.ranks = ["1", "2", "3", "4", "5", "6", "7", "8"]
+        self.ranks = [1, 2, 3, 4, 5, 6, 7, 8]
 
     """def Distribute(self):
         suite = random.choices(self.suites)
         rank = random.choices(self.ranks)
         return Card(suite[0], rank[0])"""
+    
+    def PlayRound(player, playNum, npc, npcNum):
+        pCard = player.PlayCard(playNum)
+        nCard = npc.PlayCard(npcNum)
+        if (pCard.color == nCard.color):
+            if pCard.rank < nCard.rank:
+                npc.points -= pCard.rank
+                print("Player won this round")
+            elif nCard.rank < pCard.rank:
+                player.point -= nCard.rank
+                print("NPC won this round")
+            elif nCard.rank == pCard.rank:
+                print("TIE")
+        elif (pCard.color != nCard.color):
+            if pCard.rank > nCard.rank:
+                npc.points -= pCard.rank
+                print("Player won this round")
+            elif nCard.rank > pCard.rank:
+                player.points -= nCard.rank
+                print("Npc won this round")
+            elif nCard.rank == pCard.rank:
+                player.points -= pCard.rank
+                npc.points -= nCard.rank
+                
+
+
 
 class Card:
     def __init__(self, suite, rank):
         self.suite = suite
+        if self.suite in ["Diamond", "Heart"]:
+            self.color = "red"
+        else:
+            self.color = "black"
         self.rank = rank
-        self.name = suite + " " + rank
+        self.name = suite + " " + str(rank)
 
 class Hand:
     def __init__(self):
         self.hand = []
         self.turn = True
+        self.points = 50
 
     def Draw(self, quant):
         for i in range(0, quant):
--- Cards/functions.py
+++ Cards/functions.py
@@ -4,5 +4,5 @@ import random
 
 def Distribute():
     suite = random.choices(["Diamond", "Heart", "Spade", "Club"])[0]
-    rank = random.choices(["1", "2", "3", "4", "5", "6", "7", "8"])[0]
+    rank = random.choices([1, 2, 3, 4, 5, 6, 7, 8])[0]
     return Class.Card(suite, rank)
